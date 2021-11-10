b0a9852e197365d168cd8b545d9270e32f7374cd
Nikkune <nikkune.mc@gmail.com>
2021-11-10 15:35:56 +0100

    Create/Import Base Of Website

---
+++ assets/functions/main_functions.php
@@ -0,0 +1,14 @@
+<?php
+include 'database_functions.php';
+
+$links = array(
+	["url" => "home", "name" => "Home"],
+	["url" => "musics", "name" => "Musics"],
+	["url" => "illus", "name" => "Illustrations"],
+	["url" => "members", "name" => "Members"],
+	["url" => "#", "name" => "|"],
+	["url" => "server", "name" => "Server"],
+	["url" => "news", "name" => "News"],
+	["url" => "Guide", "name" => "Guides"],
+	["url" => "#", "name" => "|"],
+	["url" => "dashboard", "name" => "Dashboard"]);
